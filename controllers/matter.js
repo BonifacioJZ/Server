2635237bbc314fbf0a2413225ade9f228ea86c8a
Bonifacio Juarez <revanjz6@gmail.com>
2018-04-16 19:15:00 -0500

    hola

--- controllers/matter.js
+++ controllers/matter.js
@@ -5,16 +5,25 @@ function postMatter(req,res) {
     console.log(req.body)
     let matter = new Matter({
         code:req.body.code,
-        mattername:req.body.namem,
+        mattername: req.body.mattername,
         alumns:[],
         descriptions:req.body.descrip
     })
 
-    Matter.save((err,matterStored)=>{
+    matter.save((err,matterStored)=>{
         if(err) res.status(500).send({message:`error al insertar materia ${err}`})
 
         res.status(200).send({matter:matterStored})
     })
+}
+function getMatters(res,req) {
+    Matter.find({},(err,matters)=>{
+        if(err) return res.status(500).send({message:`Error al encontrar las materias ${err}`})
+        if(!matters) return res.status(404).send({message:`No existen materias`})
+        res.status(200).send({matters})
+        
+    })
+    
 }
 
 
